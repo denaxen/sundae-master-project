c2cb45a07d76a2ebe63ad60bc29840ca0094d30f
Daniil Likhobaba <daniil@likhobaba.ru>
2025-03-15 17:09:18 +0100

    Update remote-development.sh

--- installation/docker-amd64-cuda/EPFL-runai-setup/example-submit-scripts/remote-development.sh
+++ installation/docker-amd64-cuda/EPFL-runai-setup/example-submit-scripts/remote-development.sh
@@ -6,14 +6,15 @@
 # For RCP use the --pvc claire-scratch:/claire-rcp-scratch
 # For IC use the runai-claire-moalla-scratch:/claire-rcp-scratch
 runai submit \
-  --name example-remote-development \
+  --name diffusion-remote-development \
   --interactive \
-  --image registry.rcp.epfl.ch/claire/moalla/diffusion-llms-project:amd64-cuda-moalla-latest \
-  --pvc runai-claire-moalla-scratch:/claire-rcp-scratch \
-  --working-dir /claire-rcp-scratch/home/moalla/diffusion-llms-project/dev \
-  -e PROJECT_ROOT_AT=/claire-rcp-scratch/home/moalla/diffusion-llms-project/dev \
+  --image registry.rcp.epfl.ch/claire/likhobab/diffusion-llms-project:amd64-cuda-likhobab-latest \
+  --pvc claire-scratch:/claire-rcp-scratch \
+  --working-dir /claire-rcp-scratch/home/likhobab/diffusion-llms-project/dev \
+  -e PROJECT_ROOT_AT=/claire-rcp-scratch/home/likhobab/diffusion-llms-project/dev \
   -e PROJECT_NAME=diffusion-llms-project \
   -e PACKAGE_NAME=diffusion_llms_project \
+  -e VSCODE_SERVER_AT=/claire-rcp-scratch/home/likhobab/remote-development/vscode-server \
   -e SSH_SERVER=1 \
    --allow-privilege-escalation \
   -g 1 --cpu 8 --cpu-limit 8 --memory 64G --memory-limit 64G --large-shm \
