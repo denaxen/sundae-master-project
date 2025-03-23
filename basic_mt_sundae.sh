89599da8202212e4622ac57797294f32aa269981
Daniil Likhobaba <daniil@likhobaba.ru>
2025-03-23 17:16:32 +0000

    sampling frequency

--- basic_mt_sundae.sh
+++ basic_mt_sundae.sh
@@ -1,3 +1,3 @@
 cd sundae
 pip install -r requirements.txt
-python main.py data=wmt14 model=sundae_diffusion_mt 'trainer.callbacks=[{_target_: callbacks.TranslationSamplingCallback, sample_frequency: 10, nb_samples: 1}]'
+python main.py data=wmt14 model=sundae_diffusion_mt 'trainer.callbacks=[{_target_: callbacks.TranslationSamplingCallback, sample_frequency: 1000, nb_samples: 1}]'
