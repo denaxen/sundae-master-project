053aa05ef5b6db6b2127510a48587b472d224671
Daniil Likhobaba <daniil@likhobaba.ru>
2025-03-23 21:32:07 +0000

    config fix

--- basic_mt_sundae.sh
+++ basic_mt_sundae.sh
@@ -1,3 +1,3 @@
 cd sundae
 pip install -r requirements.txt
-CUDA_LAUNCH_BLOCKING=1 python main.py data=wmt14 model=sundae_diffusion_mt 'trainer.callbacks=[{_target_: callbacks.TranslationSamplingCallback, sample_frequency: 1000, nb_samples: 1}]'
+CUDA_LAUNCH_BLOCKING=1 python main.py data=wmt14 model=sundae_diffusion_mt 'model.trainer.callbacks=[{_target_: callbacks.TranslationSamplingCallback, sample_frequency: 1000, nb_samples: 1}]'
