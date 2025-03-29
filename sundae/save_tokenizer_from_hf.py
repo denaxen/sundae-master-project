afe67c4244f1aea5e2a818f8dd96a8c00ee0eed1
Daniil Likhobaba <daniil@likhobaba.ru>
2025-03-29 20:08:59 +0000

    fix vocab size

--- sundae/save_tokenizer_from_hf.py
+++ sundae/save_tokenizer_from_hf.py
@@ -2,4 +2,5 @@ from transformers import AutoTokenizer
 
 
 tokenizer = AutoTokenizer.from_pretrained("speedcell4/wmt14-deen-shared-40k")
-tokenizer.save_pretrained("tokenizers/wmt14-deen-shared-40k")
+# tokenizer.save_pretrained("tokenizers/wmt14-deen-shared-40k")
+# print(tokenizer.vocab_size)
